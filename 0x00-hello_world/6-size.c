795f82ef122fdd602f66909784f4ed193e136321
Masterkiel <coolkomolafe@gmail.com>
2022-10-17 23:43:24 +0100

    Tasm gf j :wq mh ls git commit -m Tasm

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * Return: Always 0
+ */
+int main(void)
+{
+	print("Size of a char: %i byte(s)\n", sizeof(char));
+	print("Size of an int: %i byte(s)\n", sizeof(int));
+~
