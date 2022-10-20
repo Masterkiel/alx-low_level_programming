6a25bba6360d02fa4c45cb38f38bd8f1f07d5da9
Masterkiel <Coolkomolafe@gmail.com>
2022-10-19 19:20:48 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - print _putchar and a new line
+ * Return: Always 0
+ */
+int main(void)
+{
+	_patchar('_');
+	_patchar('p');
+	_patchar('u');
+	_patchar('t');
+	_patchar('c');
+	_patchar('h');
+	_patchar('a');
+	_patchar('r');
+	_putchar('\n');
+
+	return (0);
